bdfa963224b0a2a37cc6abfa616fae65ea797b04
darinetag <tagdarinefatma@gmail.com>
2023-08-11 16:47:40 +0100

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+  }
+  function fuzzyMatch(drivers, fuzzy) {
+    return drivers.filter(driver => driver.toLowerCase().startsWith(fuzzy.toLowerCase()));
+  }
+  function matchName(drivers, match) {
+    return drivers.filter(driver => driver.name === match);
+  }
+  
